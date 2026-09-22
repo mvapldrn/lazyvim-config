306bf423c4063c0d19014f669a28a98c72a5ef18
Marcel van Apeldoorn <mvapldrn@nlr.nl>
2026-09-22 11:16:59 +0200

    Updated keymaps

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # 💤 LazyVim
 
-A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
-Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+My personal [LazyVim](https://lazyvim.org) configuration based on the starter template that can be found here [LazyVim](https://github.com/LazyVim/LazyVim).
+
+## Changed keymappings
+
+I like to use the arrow keys (instead of h,j,k,l) and kitty as terminal. Kitty is my terminal and uses 
+the left-control modifier (e.g. Ctrl-Left/Right/Tab to switch between tabs). Movement between stuff inside nvim is changed slightly;
+
+| Key      | Descritpion        | Mode |
+|:--------:|:-------------------|:----:|
+| S-Left   | Go to left window  | n    |
+| S-Right  | Go to right window | n    |
+| 
+
+
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+-- Moving through windows
+keymap.set("n", "<S-Left>", "<C-w>h", { desc = "Go to Left Window" })
+keymap.set("n", "<S-Right>", "<C-w>l", { desc = "Go to Right Window" })
+keymap.set("n", "<S-Up>", "<C-w>k", { desc = "Go to Upper Window" })
+keymap.set("n", "<S-Down>", "<C-w>j", { desc = "Go to Lower Window" })
+
+-- Moving through buffers
+keymap.set("n", "<A-Left>", "<cmd>bprevious<CR>", { desc = "Go to Previous buffer" })
+keymap.set("n", "<A-Right>", "<cmd>bnext<CR>", { desc = "Go to Next buffer" })
+
+keymap.set("n", "<F4>", LazyVim.pick("live_grep"), { desc = "Grep in root dir" })
